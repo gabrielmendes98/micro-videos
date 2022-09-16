4698d47ab404bd03be389b38e22eebba43b5d0f6
Gabriel Mendes de Souza Santiago <gabrielmssantiago@gmail.com>
2022-09-16 18:54:51 +0000

    test vo.toString()

--- src/shared/domain/value-objects/__tests__/value-object.spec.ts
+++ src/shared/domain/value-objects/__tests__/value-object.spec.ts
@@ -12,4 +12,28 @@ describe('ValueObject unit tests', () => {
       prop: 'some value 2',
     });
   });
+
+  it('should convert to a string', () => {
+    const date = new Date();
+    const arrange = [
+      { received: null, expected: 'null' },
+      { received: undefined, expected: 'undefined' },
+      { received: '', expected: '' },
+      { received: 'any string', expected: 'any string' },
+      { received: 10, expected: '10' },
+      { received: 0, expected: '0' },
+      { received: true, expected: 'true' },
+      { received: false, expected: 'false' },
+      { received: date, expected: date.toString() },
+      {
+        received: { prop: 'some prop ' },
+        expected: JSON.stringify({ prop: 'some prop ' }),
+      },
+    ];
+
+    arrange.forEach((value) => {
+      const vo = new StubValueObject(value.received);
+      expect(vo + '').toBe(value.expected);
+    });
+  });
 });
