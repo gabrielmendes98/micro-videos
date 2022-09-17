8adf1ba8859ebf715084c78a60b17961393f1090
Gabriel Mendes de Souza Santiago <gabrielmssantiago@gmail.com>
2022-09-17 21:24:49 +0000

    create update, activate and deactivate methods on category

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -36,4 +36,17 @@ export class Category extends Entity<CategoryProperties> {
   get created_at() {
     return this.props.created_at;
   }
+
+  update(name: string, description: string) {
+    this.props.name = name;
+    this.props.description = description;
+  }
+
+  activate() {
+    this.props.is_active = true;
+  }
+
+  deactivate() {
+    this.props.is_active = false;
+  }
 }
