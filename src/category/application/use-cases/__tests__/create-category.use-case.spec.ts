f3d990f9a23823ef94a82db2b3d5aa5dbfbe5dd4
Gabriel Santiago <gabrielmssantiago@gmail.com>
2022-09-25 00:30:52 -0300

    create create-category usecase

---
+++ src/category/application/use-cases/__tests__/create-category.use-case.spec.ts
@@ -0,0 +1,70 @@
+import { CategoryInMemoryRepository } from 'category/infra/repositories/category-in-memory.repository';
+import { CreateCategoryUseCase } from '../create-category.use-case';
+
+describe('CreateCategoryUseCase unit tests', () => {
+  let useCase: CreateCategoryUseCase;
+  let repository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    useCase = new CreateCategoryUseCase(repository);
+  });
+
+  it('should create a category', async () => {
+    const spyInsert = jest.spyOn(repository, 'insert');
+
+    const arrange = [
+      {
+        input: { name: 'movie' },
+        output: (repository: CategoryInMemoryRepository) => ({
+          id: repository.items[0].id,
+          name: 'movie',
+          description: null,
+          is_active: true,
+          created_at: repository.items[0].created_at,
+        }),
+      },
+      {
+        input: { name: 'movie', description: 'some description' },
+        output: (repository: CategoryInMemoryRepository) => ({
+          id: repository.items[1].id,
+          name: 'movie',
+          description: 'some description',
+          is_active: true,
+          created_at: repository.items[1].created_at,
+        }),
+      },
+      {
+        input: { name: 'movie', is_active: false },
+        output: (repository: CategoryInMemoryRepository) => ({
+          id: repository.items[2].id,
+          name: 'movie',
+          description: null,
+          is_active: false,
+          created_at: repository.items[2].created_at,
+        }),
+      },
+      {
+        input: {
+          name: 'movie',
+          description: 'some description',
+          is_active: false,
+        },
+        output: (repository: CategoryInMemoryRepository) => ({
+          id: repository.items[3].id,
+          name: 'movie',
+          description: 'some description',
+          is_active: false,
+          created_at: repository.items[3].created_at,
+        }),
+      },
+    ];
+
+    for (const item of arrange) {
+      const output = await useCase.execute(item.input);
+      expect(output).toStrictEqual(item.output(repository));
+      expect(spyInsert).toHaveBeenCalledTimes(1);
+      spyInsert.mockClear();
+    }
+  });
+});
