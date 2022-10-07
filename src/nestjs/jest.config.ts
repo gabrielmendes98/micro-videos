6a4143ae48326be85b9d231232083cf6cb911859
Gabriel Mendes de Souza Santiago <gabrielmssantiago@gmail.com>
2022-10-07 14:27:04 -0300

    add nestjs

---
+++ src/nestjs/jest.config.ts
@@ -0,0 +1,20 @@
+export default {
+  displayName: {
+    name: 'nestjs',
+    color: 'magentaBright',
+  },
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^core/(.*)$': '<rootDir>/../../../node_modules/core/dist/$1',
+    '#shared/(.*)$': '<rootDir>/../../../node_modules/core/dist/shared/$1',
+    '#category/(.*)$': '<rootDir>/../../../node_modules/core/dist/category/$1',
+  },
+};
