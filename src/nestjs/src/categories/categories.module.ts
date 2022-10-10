f77767dfe3c0ac7563388a60ec55735e684f8970
Gabriel Mendes de Souza Santiago <gabrielmssantiago@gmail.com>
2022-10-09 22:11:16 -0300

    improve module providers

--- src/nestjs/src/categories/categories.module.ts
+++ src/nestjs/src/categories/categories.module.ts
@@ -1,56 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CategoriesController } from './categories.controller';
-import { CategoryInMemoryRepository } from 'core/category/infra';
-import { CategoryRepository } from 'core/category/domain';
-import {
-  CreateCategoryUseCase,
-  DeleteCategoryUseCase,
-  GetCategoryUseCase,
-  ListCategoriesUseCase,
-  UpdateCategoryUseCase,
-} from 'core/category/application';
+import { CATEGORY_PROVIDERS } from './category.providers';
+
 @Module({
   controllers: [CategoriesController],
   providers: [
-    {
-      provide: 'CategoryInMemoryRepository',
-      useClass: CategoryInMemoryRepository,
-    },
-    {
-      provide: UpdateCategoryUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.Repository) => {
-        return new UpdateCategoryUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryInMemoryRepository'],
-    },
-    {
-      provide: CreateCategoryUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.Repository) => {
-        return new CreateCategoryUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryInMemoryRepository'],
-    },
-    {
-      provide: DeleteCategoryUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.Repository) => {
-        return new DeleteCategoryUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryInMemoryRepository'],
-    },
-    {
-      provide: GetCategoryUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.Repository) => {
-        return new GetCategoryUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryInMemoryRepository'],
-    },
-    {
-      provide: ListCategoriesUseCase.UseCase,
-      useFactory: (categoryRepo: CategoryRepository.Repository) => {
-        return new ListCategoriesUseCase.UseCase(categoryRepo);
-      },
-      inject: ['CategoryInMemoryRepository'],
-    },
+    ...Object.values(CATEGORY_PROVIDERS.REPOSITORIES),
+    ...Object.values(CATEGORY_PROVIDERS.USE_CASES),
   ],
 })
 export class CategoriesModule {}
---
+++ src/nestjs/src/categories/category.providers.ts
@@ -0,0 +1,60 @@
+import { CategoryInMemoryRepository } from 'core/category/infra';
+import {
+  CreateCategoryUseCase,
+  DeleteCategoryUseCase,
+  GetCategoryUseCase,
+  ListCategoriesUseCase,
+  UpdateCategoryUseCase,
+} from 'core/category/application';
+import { CategoryRepository } from 'core/category/domain';
+
+export namespace CATEGORY_PROVIDERS {
+  export namespace REPOSITORIES {
+    export const IN_MEMORY = {
+      provide: 'CategoryInMemoryRepository',
+      useClass: CategoryInMemoryRepository,
+    };
+  }
+
+  export namespace USE_CASES {
+    export const UPDATE = {
+      provide: UpdateCategoryUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.Repository) => {
+        return new UpdateCategoryUseCase.UseCase(categoryRepo);
+      },
+      inject: [REPOSITORIES.IN_MEMORY.provide],
+    };
+
+    export const CREATE = {
+      provide: CreateCategoryUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.Repository) => {
+        return new CreateCategoryUseCase.UseCase(categoryRepo);
+      },
+      inject: [REPOSITORIES.IN_MEMORY.provide],
+    };
+
+    export const DELETE = {
+      provide: DeleteCategoryUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.Repository) => {
+        return new DeleteCategoryUseCase.UseCase(categoryRepo);
+      },
+      inject: [REPOSITORIES.IN_MEMORY.provide],
+    };
+
+    export const GET = {
+      provide: GetCategoryUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.Repository) => {
+        return new GetCategoryUseCase.UseCase(categoryRepo);
+      },
+      inject: [REPOSITORIES.IN_MEMORY.provide],
+    };
+
+    export const LIST = {
+      provide: ListCategoriesUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.Repository) => {
+        return new ListCategoriesUseCase.UseCase(categoryRepo);
+      },
+      inject: [REPOSITORIES.IN_MEMORY.provide],
+    };
+  }
+}
