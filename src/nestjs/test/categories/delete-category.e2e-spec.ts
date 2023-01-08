31e58a79a10232ef695cc96e7910d8ba3abc02a8
gabrielmendes98 <gabrielmssantiago@gmail.com>
2023-01-08 03:16:46 +0000

    implementação dos testes e2e do delete de categorias

---
+++ src/nestjs/test/categories/delete-category.e2e-spec.ts
@@ -0,0 +1,55 @@
+import request from 'supertest';
+import { Category, CategoryRepository } from 'core/category/domain';
+import { startApp } from 'src/@shared/testing/helpers';
+import { CATEGORY_PROVIDERS } from 'src/categories/category.providers';
+import { NotFoundError } from 'core/shared/domain';
+
+describe('CategoriesController (e2e)', () => {
+  describe('/delete/:id (DELETE)', () => {
+    const nestApp = startApp();
+    describe('should a response error when id is invalid or not found', () => {
+      const arrange = [
+        {
+          id: '88ff2587-ce5a-4769-a8c6-1d63d29c5f7a',
+          expected: {
+            message:
+              'Entity not found using ID 88ff2587-ce5a-4769-a8c6-1d63d29c5f7a',
+            statusCode: 404,
+            error: 'Not Found',
+          },
+        },
+        {
+          id: 'fake id',
+          expected: {
+            statusCode: 422,
+            message: 'Validation failed (uuid  is expected)',
+            error: 'Unprocessable Entity',
+          },
+        },
+      ];
+
+      test.each(arrange)('when id is $id', async ({ id, expected }) => {
+        return request(nestApp.app.getHttpServer())
+          .delete(`/categories/${id}`)
+          .expect(expected.statusCode)
+          .expect(expected);
+      });
+    });
+
+    it('should delete a category response with status 204', async () => {
+      const categoryRepo = nestApp.app.get<CategoryRepository.Repository>(
+        CATEGORY_PROVIDERS.REPOSITORIES.IN_USE.provide,
+      );
+      const category = Category.fake().aCategory().build();
+      await categoryRepo.insert(category);
+
+      await request(nestApp.app.getHttpServer())
+        .delete(`/categories/${category.id}`)
+        .expect(204);
+
+      await expect(categoryRepo.findById(category.id)).rejects.toThrow(
+        new NotFoundError(`Entity not found using ID ${category.id}`),
+      );
+    });
+  });
+});
